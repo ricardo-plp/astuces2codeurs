f695f49ccbaee221e208cb7aa6c040cedd42071b
Ricardo <tchamgouericardo@gmail.com>
2023-04-08 18:58:33 +0200

    add model Post and custom admin panel:tada:

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft','Draft'),
+        ('published', 'Published')
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CHOICES,default='draft',max_length=10)
+    publish = models.DateTimeField(default=timezone.now())
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posted')
+
+    def __str__(self):
+        return self.title
+
